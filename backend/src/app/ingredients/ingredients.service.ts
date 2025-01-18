5e6b98dd49b835f4254d26cfafff4450b899fa6a
Антонина Секрет <a.sekret@worksolutions.ru>
2025-01-18 18:09:37 +0300

    init_project: сервис таблицы ингредиентов

---
+++ backend/src/app/ingredients/ingredients.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {IngredientsEntity} from "../../common/entities/ingredients.entity";
+
+@Injectable()
+export class IngredientsService {
+    constructor(
+        @InjectRepository(IngredientsEntity)
+        private authCodeRepository: Repository<IngredientsEntity>,
+    ) {}
+}
---
+++ backend/src/common/entities/ingredients.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity({ name: "ingredients" })
+export class IngredientsEntity {
+    @PrimaryColumn({ name: "recipe_uuid", type: "varchar", nullable: false })
+    readonly recipeUuid: string;
+
+    @PrimaryColumn({ name: "product_uuid", type: "varchar", nullable: false })
+    readonly productUuid: string;
+
+    @Column({ name: "count", type: "numeric", nullable: false })
+    readonly count: number;
+}
