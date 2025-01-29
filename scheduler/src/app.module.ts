7240f918b8485e4a71504d3072e28e579efba075
Антонина Секрет <a.sekret@worksolutions.ru>
2025-01-29 21:43:19 +0300

    feature/scheduler: инициализация планировщика

---
+++ scheduler/src/app.module.ts
@@ -0,0 +1,54 @@
+import { resolve } from "path";
+
+import { Module } from "@nestjs/common";
+import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { MailerModule } from "@nestjs-modules/mailer";
+import { MailerOptions } from "@nestjs-modules/mailer/dist/interfaces/mailer-options.interface";
+import {ConfigModule} from "./common/config/config.module";
+import {ConfigService} from "./common/config/config.service";
+
+
+
+
+@Module({
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory(config: ConfigService): TypeOrmModuleOptions {
+        return {
+          type: "postgres",
+          host: config.database.host,
+          port: config.database.port,
+          username: config.database.username,
+          password: config.database.password,
+          database: config.database.database,
+          entities: [resolve(__dirname, "common", "entities", "*.entity.[t|j]s")],
+          migrations: [],
+          synchronize: false,
+          migrationsRun: true,
+          logging: true
+        };
+      }
+    }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory(config: ConfigService): MailerOptions {
+        return {
+          transport: {
+            host: config.mailerConfig.host,
+            auth: {
+              user: config.mailerConfig.user,
+              pass: config.mailerConfig.password
+            }
+          }
+        };
+      }
+    }),
+  ],
+  controllers: [],
+  providers: []
+})
+export class AppModule {}
