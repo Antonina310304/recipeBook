7a18d29ac954d42e79d670ad6b69554c126da35e
a.sekret@worksolutions.ru <a.sekret@worksolutions.ru>
2025-02-01 11:37:30 +0300

    feature/scheduler: fix типов

--- scheduler/src/app/email-message/messageFactory/types.ts
+++ scheduler/src/app/email-message/messageFactory/types.ts
@@ -1,9 +1,13 @@
 import { CommonRecipeListForMailing, DataForMailingFoKitchens } from "../../../common/repositories/recipe-events/types";
 import { EmailSendInterface } from "../types";
 
+export type SiteInterface = {
+  site: string;
+}
+
 export type MessageTypes = {
-  [MessageType.AUTHOR]?: CommonRecipeListForMailing & { site: string };
-  [MessageType.KITCHEN]?: DataForMailingFoKitchens & { site: string };
+  [MessageType.AUTHOR]?: CommonRecipeListForMailing & SiteInterface;
+  [MessageType.KITCHEN]?: DataForMailingFoKitchens & SiteInterface;
 };
 export enum MessageType {
   AUTHOR = "AUTHOR",
